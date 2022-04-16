5e59e49de0ec294bb7b14ea0af9f347b71dee952
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-04-16 19:41:36 +0300

    Selection Sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                numbers[i] = random.Next(0, 9);
+            }
+
+            SelectionSort.Sort(numbers);
+
+            Console.Write("Selection Sort");
+            Console.WriteLine(" ");
+            Console.WriteLine(string.Join(" | ", numbers));
+        }
+    }
+}
